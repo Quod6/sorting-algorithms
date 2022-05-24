a4c7000f9d49f5496e722f54bd3c30f0cafeb2b4
Quod6 <quod66@gmail.com>
2022-05-25 02:06:48 +0300

    123

--- main.cpp
+++ main.cpp
@@ -3,13 +3,14 @@
 #include <numeric>
 #include <cstdlib>
 #include <vector>
+#include <stack>
 #include <string>
 #include <iostream>
 
 using namespace std;
 
 template <typename T>
-T median(T value1, T value2, T value3)
+T findMedian(T value1, T value2, T value3)
 {
 	if ((value1 <= value2 && value2 <= value3) || (value3 <= value2 && value2 <= value1))
 	{
@@ -25,7 +26,72 @@ T median(T value1, T value2, T value3)
 }
 
 template <typename T>
-void qSort(T * array, int left, int right, int median)
+void qSort(T * array, unsigned left, unsigned right, int median)
+{
+	if ((int)(right - left) <= median)
+	{
+		T temp;
+		unsigned j;
+
+		for (unsigned i = 0; i < right + 1; i++)
+		{
+			temp = array[i];
+
+			for (j = i - 1; j >= 0 && array[j] > temp; j--)
+			{
+				array[j + 1] = array[j];
+			}
+
+			array[j + 1] = temp;
+		}
+
+		return;
+	}
+
+	T med = findMedian(array[left],
+					array[(left + right) / 2],
+					array[right]);
+
+	unsigned pivot = (left + right) / 2;
+	swap(array[med], array[pivot]);
+
+	unsigned l_hold = left;
+	unsigned r_hold = right;
+
+	while (left < right)
+	{
+		while (array[right] >= array[pivot] && left < right)
+			right--;
+
+		if (left != right)
+		{
+			array[left] = array[right];
+			left++;
+		}
+
+		while (array[left] <= array[pivot] && left < right)
+			left++;
+
+		if (left != right)
+		{
+			array[right] = array[left];
+			right--;
+		}
+	}
+
+	array[left] = array[pivot];
+	left = l_hold;
+	right = r_hold;
+
+	if (left < pivot)
+		qSort(array, left, pivot - 1, median);
+
+	if (right > pivot)
+		qSort(array, pivot + 1, right, median);
+}
+
+template <typename T>
+void qSortNoRec(T * array, int left, int right, int median)
 {
 	if (right - left <= median)
 	{
@@ -47,49 +113,49 @@ void qSort(T * array, int left, int right, int median)
 		return;
 	}
 
-	T med = median(array[left],
+	T med = findMedian(array[left],
 					array[(left + right) / 2],
 					array[right]);
 
 	swap(array[med]);
 
+	stack<int *> s;
 	int pivot;
 	int l_hold = left;
 	int r_hold = right;
 
 	pivot = array[left];
+	s.push(array);
 
-	while (left < right)
+	while(!s.empty())
 	{
-		while (array[right] >= pivot && left < right)
-			right--;
-
-		if (left != right)
+		while (left < right)
 		{
-			array[left] = array[right];
-			left++;
+			while (array[right] >= pivot && left < right)
+				right--;
+
+			if (left != right)
+			{
+				array[left] = array[right];
+				left++;
+			}
+
+			while (array[left] <= pivot && left < right)
+				left++;
+
+			if (left != right)
+			{
+				array[right] = array[left];
+				right--;
+			}
 		}
 
-		while (array[left] <= pivot && left < right)
-			left++;
-
-		if (left != right)
-		{
-			array[right] = array[left];
-			right--;
-		}
+		array[left] = pivot;
+		pivot = left;
+		left = l_hold;
+		right = r_hold;
 	}
 
-	array[left] = pivot;
-	pivot = left;
-	left = l_hold;
-	right = r_hold;
-
-	if (left < pivot)
-		qSort(array, left, pivot - 1);
-
-	if (right > pivot)
-		qSort(array, pivot + 1, right);
 }
 
 int main()
@@ -100,7 +166,7 @@ int main()
 		cout << a[i] << ' ';
 
 	cout << endl;
-	qSort(a, 0, 16);
+	qSort(a, 0, 16, 15);
 
 	for (int i = 0; i < 17; i++)
 		cout << a[i] << ' ';
